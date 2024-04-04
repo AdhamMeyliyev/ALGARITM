ca596ff40df1f5580d70cb3f23ec9682555f1445
AdhamMeyliyev <adhammeyliyev8@gmail.com>
2024-04-04 17:18:17 +0500

    algaritm

---
+++ index.js
@@ -0,0 +1,51 @@
+
+//1
+// let arr = ["home", "joke", "alibobo"]
+
+// let a = arr[arr.length -1 ].length
+// console.log(a);
+
+
+
+
+//2
+// let obj = {
+//     1:"salim",
+//     2:"ali",
+//     10:"vali",
+//     12:"vali"
+// }
+
+// let key = Object.keys(obj);
+// let sum = 0;
+
+// for(let i=0; i<key.length; i++){
+//     sum += parseInt(key[i])
+// }
+
+// console.log(sum);
+
+
+
+//3
+
+// let input = "1.1.1"
+// let output = input.replace(/\./g , "[.]")
+
+// console.log(output);
+
+
+
+// 4
+
+// let n=6;
+
+// function logarifm (n){
+//     if ( n === 0 || n === 1){
+//         return 1;
+//     }else{
+//         return n * logarifm(n-1)
+//     }
+// }
+
+// console.log(logarifm(n));
